112a6e5bfefd24749f3264b56e2caf3efb2a4371
Marco Crespi <mrc@flxlabs.com>
2017-12-21 18:30:31 +0100

    feat(references): Add information about what part of the templates/code references a certain translation

--- code/tasks/CollectTranslationsTask.php
+++ code/tasks/CollectTranslationsTask.php
@@ -9,29 +9,88 @@ class CollectTranslationsTask extends BuildTask {
 		"/<%t\s([\w.]*)\s?(?:['\"](.*?)['\"])?[\w$=. ]*\s?%>/iu";
 	protected static $styleSheet = '<style>
 		html, body {
-			width: 100%;
 		}
-		div {
+		.code {
+			width: 100%;
+			white-space: pre;
+			font-family: monospace;
+		}
+		.info {
 			position: relative;
-			display: inline-block;
 			margin: 0;
 			padding: 0;
 			width: 100%;
-			background: #EEEEEE;
+			background: #ddd;
 		}
-		div:hover {
-			background: #CCCCCC;
+		.info:hover {
+			background: #bbb;
+		}
+		.info:hover .tooltip {
+			visibility: visible;
+		}
+		.tooltip {
+			position: absolute;
+			top: 100%;
+			left: 20%;
+			right: 0;
+			z-index: 100;
+			background-color: white;
+			border: 1px solid black;
+			color: black;
+			padding: 2px;
+			visibility: hidden;
+			box-sizing: border-box;
 		}
 		.removed {
 			color: red;
 		}
 		.uncertain {
-			color: orange;
+			color: #EE7600;
 		}
 		.new {
 			color: green;
 		}
+		table {
+			border-collapse: collapse;
+		}
+		td, th {
+			padding: 0.5em;
+		}
 	</style>';
+	protected static $javascript = '<script>
+	function toggleDeleted() {
+		var elems = document.getElementsByClassName("removed");
+		if (elems.length === 0) return;
+
+		var mode = elems[0].style.display === "none" ? "block" : "none";
+		for (var i = 0; i < elems.length; i++) {
+			elems[i].style.display = mode;
+		}
+	}
+	var orig = "";
+	var copy = false;
+	function toggleCopy() {
+		if (copy) {
+			document.getElementsByClassName("code")[0].innerHTML = orig;
+			copy = false;
+			return;
+		}
+
+		orig = document.getElementsByClassName("code")[0].innerHTML;
+
+		var elems = document.getElementsByClassName("tooltip");
+		while (elems.length > 0) {
+			elems[0].outerHTML = "";
+		}
+
+		var elems = document.getElementsByClassName("info");
+		while (elems.length > 0) {
+			elems[0].classList.remove("info");
+		}
+
+		copy = true;
+	}
+	</script>';
 
 	protected $title = 'Collect Translations Task';
 	protected $description = '
@@ -94,29 +153,31 @@ class CollectTranslationsTask extends BuildTask {
 
 			self::compareArrays($orig, $new, $vars);
 
-			self::ksortRecursive($orig);
+			self::ksortRecursive($new);
 
-			$string = htmlentities(Yaml::dump($orig, 10, 2));
-			$lines = mb_split("\n", $string);
-			foreach ($lines as &$line) {
-				if (mb_strpos($line, "[-]")) {
-					$line = "<div class='removed'>" . 
-						mb_ereg_replace(preg_quote("[-] "), "", $line) . "</div>";
-				} else if (mb_strpos($line, "[*]")) {
-					$line = "<div class='uncertain'>" . 
-						mb_ereg_replace(preg_quote("[*] "), "", $line) . "</div>";
-				} else if (mb_strpos($line, "[+]")) {
-					$line = "<div class='new'>" . $line . "</div>";
-				}
+			echo self::$styleSheet;
+			echo self::$javascript;
+
+			if (count($vars) > 0) {
+				echo "<b>The following variables were found, make sure to include";
+				echo "all possible values as entries in the translation file aswell:</b>";
+				echo "<table><thead><tr><th>Variable</th><th>File</th>";
+				echo "<th>Line #</th><th>Line</th></thead><tbody>";
+				self::renderVariables($vars);
+				echo "</tbody></table><br><br>";
 			}
-			$string = implode("\n", $lines);
 
-			echo self::$styleSheet . "<pre>" . $string . "</pre>";
+			echo "<button onclick='toggleDeleted()'>Toggle deleted elements</button><br><br>";
+			echo "<button onclick='toggleCopy()'>Toggle copy mode</button><br><br>";
+			
+			echo "<div class='code'>";
+			echo self::render($new);
+			echo "</div>";
 		} else {
 			$locale = mb_substr(i18n::get_locale(), 0, 2);
 			$orig = array($locale => $arr);
 
-			echo "<pre>" . htmlentities(Yaml::dump($orig, 10, 2)) . "</pre>";
+			echo "<div class='code'>" . htmlentities(Yaml::dump($orig, 10, 2)) . "</div>";
 		}
 	}
 
@@ -173,6 +234,7 @@ class CollectTranslationsTask extends BuildTask {
 								"lineNr" => $fli + 1,
 								"info" => "Skipping because key could not be found",
 							));
+							continue;
 						} else {
 							$path = array_values(mb_split("\.", $matches[1]));
 							$value = count($matches) > 2 ? $matches[2] : "";
@@ -184,7 +246,15 @@ class CollectTranslationsTask extends BuildTask {
 							$k = trim($key);
 							$temp = &$temp[$k];
 						}
-						$temp = $value;
+						//$temp = $value;
+						if (!isset($temp))
+							$temp = array("__values__" => true);
+						$temp[] = array(
+							"file" => basename($file->getPathName()),
+							"line" => trim(mb_ereg_replace("\n", "", $fl)),
+							"lineNr" => $fli + 1,
+							"val" => $value,
+						);
 
 						// Go to next possible translation in the same line
 						$in = mb_substr($in, $pos + 3);
@@ -200,7 +270,7 @@ class CollectTranslationsTask extends BuildTask {
 						$keyEndPos = mb_strpos($fl, ")", $pos + 3);
 						if ($keyEndPos === false) {
 							var_dump(array(
-								"file" => $file->getPathName(),
+								"file" => basename($file->getPathName()),
 								"line" => trim(mb_ereg_replace("\n", "", $fl)),
 								"lineNr" => $fli + 1,
 								"info" => "Skipping because key end could not be found",
@@ -219,11 +289,14 @@ class CollectTranslationsTask extends BuildTask {
 
 					if (mb_strpos($key, "$") !== false) {
 						preg_match(self::$varLine, $key, $matches);
+
 						if (count($matches) > 1) {
 							$newKey = mb_ereg_replace(preg_quote($matches[0]), "*", $key);
 							$keySplits = mb_split("\.", $newKey);
 
 							// Insert this variable into the variable array
+							// so that we can warn the user about removed keys
+							// that might still be used by variables
 							$temp = &$vars;
 							foreach ($keySplits as $subKey) {
 								$k = trim($subKey);
@@ -232,15 +305,24 @@ class CollectTranslationsTask extends BuildTask {
 								}
 								$temp = &$temp[$k];
 							}
-							$temp = $matches[1];
+							// Set the entry in the variable array to this variable
+							if (!isset($temp)) {
+								$temp = array("__values__" => true);
+							}
+							$temp[] = array(
+								"file" => basename($file->getPathName()),
+								"line" => trim(mb_ereg_replace("\n", "", $fl)),
+								"lineNr" => $fli + 1,
+								"var" => $matches[1],
+							);
+						} else {
+							var_dump(array(
+								"file" => $file->getPathName(),
+								"line" => trim(mb_ereg_replace("\n", "", $fl)),
+								"lineNr" => $fli + 1,
+								"info" => "Skipping because it might contain a variable",
+							));
 						}
-
-						var_dump(array(
-							"file" => $file->getPathName(),
-							"line" => trim(mb_ereg_replace("\n", "", $fl)),
-							"lineNr" => $fli + 1,
-							"info" => "Skipping because it might contain a variable",
-						));
 					} else {
 						$path = array_values(array_filter(mb_split("\.", $key)));
 
@@ -250,7 +332,15 @@ class CollectTranslationsTask extends BuildTask {
 							$k = trim($key);
 							$temp = &$temp[$k];
 						}
-						$temp = $value;
+						//$temp = $value;
+						if (!isset($temp))
+							$temp = array("__values__" => true);
+						$temp[] = array(
+							"file" => basename($file->getPathName()),
+							"line" => trim(mb_ereg_replace("\n", "", $fl)),
+							"lineNr" => $fli + 1,
+							"val" => trim($value),
+						);
 					}
 				}
 			}
@@ -262,31 +352,147 @@ class CollectTranslationsTask extends BuildTask {
 		);
 	}
 
-	private static function compareArrays(&$orig, &$new, $vars) {
-		foreach ($new as $key => $value) {
-			if (!array_key_exists($key, $orig)) {
-				self::setValue($key, $orig, $new);
-			} else if (is_array($value)) {
-				$newVars = array_key_exists("*", $vars) ? $vars : 
-					(array_key_exists($key, $vars) ? $vars[$key] : array());
-				self::compareArrays($orig[$key], $value, $newVars);
+	private static function render($arr, $level = 0) {
+		$indent = str_repeat(" ", $level * 2);
+
+		$string = "";
+		foreach ($arr as $key => $value) {
+			if ($key === "__-__") continue;
+			if ($key === "__+__") continue;
+			if ($key === "__*__") continue;
+
+			$hasMod = false;
+			$hasValues = isset($value["__values__"]);
+
+			if (isset($value["__-__"])) {
+				$string .= "<div class='".($hasValues ? "info ": "")."removed'>";
+				$string .= "<div class='tooltip'><b>No references</b></div>";
+				$hasMod = true;
+			} else if (isset($value["__+__"])) {
+				$string .= "<div class='".($hasValues ? "info " : "")."new'>";
+				if ($hasValues) {
+					$tblString = "<b>Introduced by the following line(s):</b><br><br>" . 
+						"<table><thead><tr><th>File</th><th>Line #</th>" . 
+						"<th>Line</th></tr></thead><tbody>";
+					foreach ($value as $vals) {
+						if (!is_array($vals)) continue;
+						$line = htmlentities($vals["line"]);
+						$tblString .= "<tr><td>{$vals['file']}</td>" . 
+							"<td>{$vals['lineNr']}</td><td>{$line}</td></tr>";
+					}
+					$string .= "<div class='tooltip'>$tblString</tbody></table></div>";
+				}
+				$hasMod = true;
+			} else if (isset($value["__*__"])) {
+				$string .= "<div class='".($hasValues ? "info ": "")."uncertain'>";
+				if ($hasValues) {
+					$tblString = "<b>No explicit reference, but possibly used by following variables:</b><br><br>" . 
+						"<table><thead><tr><th>Variable</th><th>File</th><th>Line #</th>" . 
+						"<th>Line</th></tr></thead><tbody>";
+					foreach ($value as $vals) {
+						if (!is_array($vals)) continue;
+						$line = htmlentities($vals["line"]);
+						$tblString .= "<tr><td>{$vals['var']}</td><td>{$vals['file']}</td>" . 
+							"<td>{$vals['lineNr']}</td><td>{$line}</td></tr>";
+					}
+					$string .= "<div class='tooltip'>$tblString</tbody></table></div>";
+				}
+				$hasMod = true;
+			} else if ($hasValues) {
+				$string .= "<div class='info'>";
+				$tblString = "<b>Used by following lines:</b><br><br>" . 
+					"<table><thead><tr><th>File</th><th>Line #</th>" . 
+					"<th>Line</th></tr></thead><tbody>";
+				foreach ($value as $vals) {
+					if (!is_array($vals)) continue;
+					$line = htmlentities($vals["line"]);
+					$tblString .= "<tr><td>{$vals['file']}</td>" . 
+						"<td>{$vals['lineNr']}</td><td>{$line}</td></tr>";
+				}
+				$string .= "<div class='tooltip'>$tblString</tbody></table></div>";
+				$hasMod = true;
+			}
+
+			$string .= "{$indent}{$key}:";
+			$val = isset($value["__orig__"]) ? $value["__orig__"] : 
+				(isset($value["__values__"]) ? $value[0]["val"] : $value);
+
+			if (is_array($val)) {
+				$string .= ($hasMod ? "</div>" : "\n") . self::render($val, $level + 1);
+			} else {
+				$yaml = Yaml::dump((isset($value["__+__"]) ? "[+] " : "") . $val);
+				$string .= " " . htmlentities($yaml) . ($hasMod ? "</div>" : "\n");
 			}
 		}
-		foreach ($orig as $key => $value) {
-			if (!array_key_exists($key, $new)) {
-				$type = array_key_exists("*", $vars) ? "[*] " : "[-] ";
-				$orig[$key] = $type . print_r($orig[$key], true);
+		return $string;
+	}
+
+	private static function renderVariables($vars) {
+		foreach ($vars as $key => $var) {
+			if (is_int($key)) {
+				echo "<tr><td>{$var['var']}</td><td>{$var['file']}</td>";
+				echo "<td>{$var['lineNr']}</td><td>{$var['line']}</td></tr>";
+			} else {
+				self::renderVariables($var);
 			}
 		}
 	}
-	private static function setValue($key, &$orig, &$new) {
-		if (is_array($new[$key])) {
-			$orig[$key] = array();
-			foreach ($new[$key] as $subKey => $value) {
-				self::setValue($subKey, $orig[$key], $new[$key]);
+
+	private static function compareArrays(&$orig, &$new, $vars) {
+		// Check the new array for any keys that don't occur in the old array
+		// and mark them and any subkeys as new
+		foreach ($new as $key => $value) {
+			if ($key === "__+__") continue;
+			if ($key === "__-__") continue;
+			if ($key === "__*__") continue;
+
+			if ($orig === null || !array_key_exists($key, $orig)) {
+				$new[$key]["__+__"] = true;
 			}
-		} else {
-			$orig[$key] = "[+] " . $new[$key];
+			if (isset($new[$key]["__values__"])) {
+				if (isset($orig[$key])) {
+					$new[$key]["__orig__"] = $orig[$key];
+				}
+			} else {
+				$newVars = array_key_exists("*", $vars) ? $vars : 
+					(array_key_exists($key, $vars) ? $vars[$key] : array());
+				self::compareArrays($orig[$key], $new[$key], $newVars);
+			}
+		}
+
+		// If the original didn't have this key then we can exit here
+		if (!$orig) {
+			return;
+		}
+
+		// Check the old array for any values that aren't present in the new
+		// array, and mark them as either deleted or unsure, depending on if
+		// we can find a variable that might refernce that translation key
+		foreach ($orig as $key => $value) {
+			if (!array_key_exists($key, $new)) {
+				$refs = array_key_exists("*", $vars) ? $vars["*"] : array();
+
+				// If the original is an array then we have to go mark all the
+				// sub keys as missing
+				if (is_array($orig[$key])) {
+					$new[$key] = array();
+					$new[$key][(count($refs) > 0 ? "__*__" : "__-__")] = true;
+					$newVars = array_key_exists("*", $vars) ? $vars : 
+						(array_key_exists($key, $vars) ? $vars[$key] : array());
+					self::compareArrays($orig[$key], $new[$key], $newVars);
+				} else {
+					if (count($refs) > 0) {
+						$new[$key] = $refs;
+						$new[$key]["__*__"] = true;
+						$new[$key]["__values__"] = true;
+						$new[$key][0]["val"] = $orig[$key];
+					} else {
+						$new[$key]["__-__"] = true;
+						$new[$key]["__values__"] = true;
+						$new[$key][0]["val"] = $orig[$key];
+					}
+				}
+			}
 		}
 	}
 }
